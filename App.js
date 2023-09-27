782e94fafb29282e3e188a72ad3f6a0cd7b4415c
mphoChuene <mphochuene42@gmail.com>
2023-09-27 15:09:43 +0200

    completed dependencies

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import AppNavigation from "./navigation/appNavigation";
+import AppNavigation from './navigation/appNavigation';
 
 export default function App() {
-  return <AppNavigation />;
+  return (
+    <AppNavigation />
+  );
 }
